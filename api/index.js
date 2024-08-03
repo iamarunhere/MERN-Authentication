3c8e8f25ff7006b2ae9e3f359367772feb0e9799
your_username <your_email@example.com>
2024-08-03 09:47:19 +0530

    connected to mongo db

--- api/index.js
+++ api/index.js
@@ -1,4 +1,7 @@
 import express from "express";
+import { connect } from "./connectDb.js";
 const app = express();
 const PORT = 3000;
-app.listen(PORT, console.log(`server is running on ${PORT}`));
+
+connect(); //function to connect to mongo db
+app.listen(PORT, () => console.log(`server is running on ${PORT}`));
