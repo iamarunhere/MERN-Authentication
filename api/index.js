4facded937d86b8a01ba7dc72c6b335cd333e609
your_username <your_email@example.com>
2024-08-03 11:00:34 +0530

    created model view controller framework and tested it

--- api/index.js
+++ api/index.js
@@ -1,7 +1,10 @@
 import express from "express";
 import { connect } from "./connectDb.js";
+import userRoute from "./routes/userRoute.js";
 const app = express();
 const PORT = 3000;
 
 connect(); //function to connect to mongo db
+
+app.use("/api/user/", userRoute);
 app.listen(PORT, () => console.log(`server is running on ${PORT}`));
