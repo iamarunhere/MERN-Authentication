862193ce83d364b4a24ddafdd3c9fb3949257dd7
your_username <your_email@example.com>
2024-08-04 05:17:33 +0530

    completed sign in api functionality

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -1,5 +1,6 @@
 import bcrypt from "bcrypt";
 import User from "./../models/userModel.js";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -10,6 +11,26 @@ export const signup = async (req, res, next) => {
     await newUser.save();
     res.status(201).json({ message: "User Created Succesfully" });
     console.log("User Created Succesfully");
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const user = await User.findOne({ email });
+    if (!user) return res.status(404).json("User not found");
+    const validPassword = await bcrypt.compare(password, user.password);
+    if (!validPassword) return res.status(401).json("Inavlid Credentials");
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+    const { password: hashedPassword, ...rest } = user._doc;
+    const expiryDate = new Date(Date.now() + 3600000);
+
+    res
+      .cookie("access_token", token, { httpOnly: true, expires: expiryDate })
+      .status(200)
+      .json(rest);
   } catch (error) {
     next(error);
   }
