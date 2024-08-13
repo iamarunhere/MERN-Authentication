b4390c19d5d61f1cd7ed5486719af8101acbe7cd
your_username <your_email@example.com>
2024-08-14 01:26:37 +0530

    changed api url in OAuth component

--- client/src/components/OAuth.jsx
+++ client/src/components/OAuth.jsx
@@ -12,18 +12,21 @@ const OAuth = () => {
       const provider = new GoogleAuthProvider();
       const auth = getAuth(app);
       const result = await signInWithPopup(auth, provider);
-      const res = await fetch("http://localhost:3000/api/auth/google", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        credentials: "include",
-        body: JSON.stringify({
-          name: result.user.displayName,
-          email: result.user.email,
-          photo: result.user.photoURL,
-        }),
-      });
+      const res = await fetch(
+        "https://mern-authentication-3qy1.onrender.com/api/auth/google",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          credentials: "include",
+          body: JSON.stringify({
+            name: result.user.displayName,
+            email: result.user.email,
+            photo: result.user.photoURL,
+          }),
+        }
+      );
 
       const data = await res.json();
       console.log(data);
